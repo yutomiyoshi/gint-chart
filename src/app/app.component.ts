a82429b682b1a0a579d026442b672657c45ff20a
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-02 22:25:35 +0900

    [add][sample]ファイルの読み書きのUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+declare global {
+    interface Window {
+        electronAPI: {
+            readTextFile: (filePath: string) => Promise<string>;
+            writeTextFile: (filePath: string, content: string) => Promise<string>;
+        };
+    }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'gint-chart';
+  filePath: string = './mydata.txt'; // アプリケーションのルートからの相対パスなどを指定
+    fileContent: string = '';
+
+    constructor() {}
+
+    async readFile() {
+        try {
+            this.fileContent = await window.electronAPI.readTextFile(this.filePath);
+            alert('ファイルが読み込まれました！');
+        } catch (error: any) {
+            if (error instanceof Error) {
+                alert('ファイルの読み込みに失敗しました: ' + error.message);
+            } else {
+                alert('ファイルの読み込みに失敗しました: 不明なエラー');
+            }
+        }
+    }
+
+    async writeFile() {
+        try {
+            await window.electronAPI.writeTextFile(this.filePath, this.fileContent);
+            alert('ファイルが書き込まれました！');
+        } catch (error: any) {
+          if (error instanceof Error) {
+            alert('ファイルの書き込みに失敗しました: ' + error.message);
+          } else {
+            alert('ファイルの書き込みに失敗しました: 不明なエラー');
+          }
+        }
+    }
 }
