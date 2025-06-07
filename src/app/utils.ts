8ddd39c10c23131e4e9b0d5b5445f2b2ee6992be
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-07 16:17:48 +0900

    [add]アサーション関数

---
+++ src/app/utils.ts
@@ -0,0 +1,6 @@
+// nullやundefinedを判定するユーティリティ関数
+export function isNullOrUndefined<T>(
+  value: T | null | undefined
+): value is null | undefined {
+  return value === null || value === undefined;
+}
