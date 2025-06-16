bbe55dbef48ace207830dbae18cc9178e59e1530
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-16 21:23:18 +0900

    [update]デフォルトの画面サイズ

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ let win;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: false, // セキュリティのためにfalseを推奨
       contextIsolation: true, // セキュリティのためにtrueを推奨
