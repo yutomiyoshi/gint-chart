027775eeb3f1c445d0abbbaf0f4fbf0e3c242869
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-16 16:56:38 +0900

    [update] Refactor app component to manage subscriptions and integrate issue detail dialog expansion service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IssuesStoreService } from '@src/app/store/issues-store.service';
 import { GitLabConfigStoreService } from '@src/app/store/git-lab-config-store.service';
+import { Subscription } from 'rxjs';
+import { IssueDetailDialogExpansionService } from './issue-detail-dialog/issue-detail-dialog-expansion.service';
 
 @Component({
   selector: 'app-root',
@@ -8,27 +10,43 @@ import { GitLabConfigStoreService } from '@src/app/store/git-lab-config-store.se
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadingOverlay = true;
   isShowTitle = true;
   isShowStatus = true;
+  private subscription = new Subscription();
 
   constructor(
     private issueStore: IssuesStoreService,
-    private gitLabConfigStore: GitLabConfigStoreService
+    private gitLabConfigStore: GitLabConfigStoreService,
+    private issueDetailDialogExpansionService: IssueDetailDialogExpansionService
   ) {}
 
   ngOnInit() {
-    this.gitLabConfigStore.loadConfig().subscribe({
-      // error: () => {}, //サービス側からエラーハンドリングするため不要
-      next: () => {
-        this.issueStore.syncAllIssues().subscribe({
-          // error: () => {}, //サービス側からエラーハンドリングするため不要
-          next: () => {
-            this.loadingOverlay = false;
-          },
-        });
-      },
-    });
+    this.subscription.add(
+      this.issueDetailDialogExpansionService.expandedIssueId$.subscribe({
+        next: (issueId: number | undefined) => {
+          console.log(issueId);
+        },
+      })
+    );
+
+    this.subscription.add(
+      this.gitLabConfigStore.loadConfig().subscribe({
+        // error: () => {}, //サービス側からエラーハンドリングするため不要
+        next: () => {
+          this.issueStore.syncAllIssues().subscribe({
+            // error: () => {}, //サービス側からエラーハンドリングするため不要
+            next: () => {
+              this.loadingOverlay = false;
+            },
+          });
+        },
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
