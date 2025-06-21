a4f73a4226c5f007a9e21b38812a37061d70929c
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-21 21:58:16 +0900

    [add]マイルストーンの型

---
+++ src/app/model/milestone.model.ts
@@ -0,0 +1,14 @@
+export interface Milestone {
+  id: number;
+  iid: number;
+  project_id: number;
+  title: string;
+  description: string;
+  state: string;
+  created_at: string;
+  updated_at: string;
+  due_date: string | null;
+  start_date: string | null;
+  expired: boolean;
+  web_url: string;
+}
