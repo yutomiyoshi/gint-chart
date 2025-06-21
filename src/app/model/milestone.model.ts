46a66d70144295ed56ffd38252c4377cbd363f3d
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-21 22:07:13 +0900

    [add]GitLab APIのマイルストーン型と内部クラスを変換

--- src/app/model/milestone.model.ts
+++ src/app/model/milestone.model.ts
@@ -1,14 +1,61 @@
+import { GitLabApiMilestone } from '../git-lab-api/git-lab-milestone.model';
+import { isNull, isUndefined } from '../utils/utils';
+
 export interface Milestone {
+  /**
+   * id
+   * @type {number}
+   */
   id: number;
-  iid: number;
+
+  /**
+   * 所属プロジェクトのid
+   * @type {number}
+   */
   project_id: number;
+
+  /**
+   * タイトル
+   * @type {string}
+   */
   title: string;
+
+  /**
+   * 説明
+   * @type {string}
+   */
   description: string;
+
+  /**
+   * 状態
+   * @type {string}
+   */
   state: string;
-  created_at: string;
-  updated_at: string;
-  due_date: string | null;
-  start_date: string | null;
-  expired: boolean;
-  web_url: string;
+
+  /**
+   * 期限日
+   * @type {Date | undefined}
+   */
+  due_date: Date | undefined;
+}
+
+/**
+ * GitLab APIのmilestoneレスポンス型からMilestone型へ変換する関数
+ * @param apiMilestone GitLab APIのmilestoneレスポンス型
+ * @returns Milestone型
+ */
+export function convertJsonToMilestone(
+  apiMilestone: GitLabApiMilestone
+): Milestone {
+  return {
+    id: apiMilestone.id,
+    project_id: apiMilestone.project_id,
+    title: apiMilestone.title,
+    description: apiMilestone.description,
+    state: apiMilestone.state,
+    due_date:
+      isUndefined(apiMilestone.due_date) || isNull(apiMilestone.due_date)
+        ? undefined
+        : new Date(apiMilestone.due_date),
+  };
 }
