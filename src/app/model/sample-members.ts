3f7758b67804aa8b8fb2af77505c75b0cba801f6
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-29 02:50:13 +0900

    [add]サンプルメンバー

---
+++ src/app/model/sample-members.ts
@@ -0,0 +1,54 @@
+import { Member } from '@src/app/model/member.model';
+
+export const SAMPLE_MEMBERS: Member[] = [
+  {
+    id: 1,
+    username: 'tanaka_taro',
+    name: '田中太郎',
+  },
+  {
+    id: 2,
+    username: 'sato_hanako',
+    name: '佐藤花子',
+  },
+  {
+    id: 3,
+    username: 'suzuki_ichiro',
+    name: '鈴木一郎',
+  },
+  {
+    id: 4,
+    username: 'yamada_jiro',
+    name: '山田次郎',
+  },
+  {
+    id: 5,
+    username: 'takahashi_saburo',
+    name: '高橋三郎',
+  },
+  {
+    id: 6,
+    username: 'ito_shiro',
+    name: '伊藤四郎',
+  },
+  {
+    id: 7,
+    username: 'watanabe_goro',
+    name: '渡辺五郎',
+  },
+  {
+    id: 8,
+    username: 'kobayashi_rokuro',
+    name: '小林六郎',
+  },
+  {
+    id: 9,
+    username: 'kato_shichiro',
+    name: '加藤七郎',
+  },
+  {
+    id: 10,
+    username: 'yoshida_hachiro',
+    name: '吉田八郎',
+  },
+];
