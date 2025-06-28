81cc289f973ec4f64854a2bdd0bce56358fe07bb
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-29 02:50:32 +0900

    [add]メンバーストア

---
+++ src/app/store/member-store.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { GitLabApiService } from '@src/app/git-lab-api/git-lab-api.service';
+import {
+  Member,
+  convertGitLabMemberToMember,
+} from '@src/app/model/member.model';
+import { GitLabMember } from '@src/app/git-lab-api/git-lab-member.model';
+import { BehaviorSubject, from, Observable, tap } from 'rxjs';
+import { isDebug } from '@src/app/debug';
+import { GitLabConfigStoreService } from '@src/app/store/git-lab-config-store.service';
+import { isNull } from '@src/app/utils/utils';
+import { SAMPLE_MEMBERS } from '@src/app/model/sample-members';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberStoreService {
+  private membersSubject = new BehaviorSubject<Member[]>([]);
+  public members$: Observable<Member[]> = this.membersSubject.asObservable();
+
+  constructor(
+    private readonly gitlabApi: GitLabApiService,
+    private readonly gitlabConfigStore: GitLabConfigStoreService
+  ) {}
+
+  /**
+   * グループメンバーを同期
+   * @returns メンバーリストのObservable
+   */
+  syncMembers(): Observable<Member[]> {
+    if (isDebug) {
+      this.membersSubject.next(SAMPLE_MEMBERS);
+      return from([SAMPLE_MEMBERS]);
+    }
+
+    const config = this.gitlabConfigStore.config;
+    const groupId = config.groupId;
+    if (isNull(groupId)) {
+      this.membersSubject.next([]);
+      return from([[]]);
+    }
+
+    // グループメンバー取得
+    return this.gitlabApi
+      .fetchGroup<GitLabMember, Member>(
+        groupId,
+        'members',
+        convertGitLabMemberToMember
+      )
+      .pipe(
+        tap((members) => {
+          this.membersSubject.next(members);
+        })
+      );
+  }
+
+  /**
+   * メンバーをIDから取得
+   * @param id メンバーID
+   * @returns メンバー
+   */
+  findMemberById(id: number): Member | undefined {
+    const current = this.membersSubject.getValue();
+    return current.find((member) => member.id === id);
+  }
+
+  /**
+   * メンバーをユーザー名から取得
+   * @param username ユーザー名
+   * @returns メンバー
+   */
+  findMemberByUsername(username: string): Member | undefined {
+    const current = this.membersSubject.getValue();
+    return current.find((member) => member.username === username);
+  }
+
+  /**
+   * メンバーを名前から取得
+   * @param name 名前
+   * @returns メンバー
+   */
+  findMemberByName(name: string): Member | undefined {
+    const current = this.membersSubject.getValue();
+    return current.find((member) => member.name === name);
+  }
+}
