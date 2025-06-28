0d1d441aabc19895667687755854d12ecff1de30
yutomiyoshi <seanayuuto0826@gmail.com>
2025-06-28 13:03:51 +0900

    [add]コンバート関数

--- src/app/model/label.model.ts
+++ src/app/model/label.model.ts
@@ -1,3 +1,5 @@
+import { GitLabLabel } from '../git-lab-api/git-lab-label.model';
+
 export interface Label {
   /**
    * id
@@ -22,4 +24,13 @@ export interface Label {
    * @type {string}
    */
   color: string;
+}
+
+export function convertGitLabLabelToLabel(gitLabLabel: GitLabLabel): Label {
+  return {
+    id: gitLabLabel.id,
+    name: gitLabLabel.name,
+    description: gitLabLabel.description ?? '',
+    color: gitLabLabel.color,
+  };
 }
