32aab5a376680ed5f789bfa7426c85f005677168
yutomiyoshi <seanayuuto0826@gmail.com>
2025-07-03 21:55:09 +0900

    [add]ビュー設定のコンポーネント

---
+++ src/app/view-settings/view-settings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-settings',
+  imports: [],
+  templateUrl: './view-settings.component.html',
+  styleUrl: './view-settings.component.scss'
+})
+export class ViewSettingsComponent {
+
+}
